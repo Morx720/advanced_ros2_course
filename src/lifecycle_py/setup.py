65cd1683dfc0a6b46c7f7359d5a82cce63f102bd
mohamed <afandimohammad362@gmail.com>
2024-09-10 09:56:55 +0200

    step3

--- src/lifecycle_py/setup.py
+++ src/lifecycle_py/setup.py
@@ -22,7 +22,8 @@ setup(
         'console_scripts': [
             "number_publisher = lifecycle_py.number_publisher:main",
             "lifecycle_node_manager=lifecycle_py.lifecycle_node_manager:main",
-            "move_to_position_server=lifecycle_py.move_to_position_server:main"
+            "move_to_position_server=lifecycle_py.move_to_position_server:main",
+            "move_to_position_startup=lifecycle_py.move_to_position_startup:main"
         ],
     },
 )
